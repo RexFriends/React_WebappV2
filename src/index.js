a893bb5efe1fac566f7b7c5ad796f32d368a41f7
Wesley Chen <weschen.github@gmail.com>
2021-03-01 20:41:25 -0500

    update

--- src/index.js
+++ src/index.js
@@ -3,31 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
-// import firebase from "firebase";
+import firebase from "firebase";
 
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA0C_6O04biBpvVC579SBzGSUQ_IY2bF4I",
-//   authDomain: "rexfriends-dev.firebaseapp.com",
-//   projectId: "rexfriends-dev",
-//   storageBucket: "rexfriends-dev.appspot.com",
-//   messagingSenderId: "581619494498",
-//   appId: "1:581619494498:web:ec5b8f17fbfe63f1252b3b",
-//   measurementId: "G-MJ0ZRL7M0M"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyA0C_6O04biBpvVC579SBzGSUQ_IY2bF4I",
+  authDomain: "rexfriends-dev.firebaseapp.com",
+  projectId: "rexfriends-dev",
+  storageBucket: "rexfriends-dev.appspot.com",
+  messagingSenderId: "581619494498",
+  appId: "1:581619494498:web:ec5b8f17fbfe63f1252b3b",
+  measurementId: "G-MJ0ZRL7M0M"
+};
 
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-// if (firebaseApp != null) {
-//   console.log('firebase app started successfully');
-// } else {
-//   console.log('firebase app failed to start');
-// }
+if (firebaseApp != null) {
+  console.log('firebase app started successfully');
+} else {
+  console.log('firebase app failed to start');
+}
 
-// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
 
 ReactDOM.render(
