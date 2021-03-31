0565877e1ab88b2c58685ed2cb85ee8c50e16735
James Lowe <james.lowe@fixdapp.com>
2021-03-30 21:35:12 -0400

    updating settings

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 import './Setting.scss'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import {  useHistory, Redirect }from "react-router-dom"
 function Setting(){
     let history = useHistory()
-    
+    const [username, usernameSet] = useState("")
+    const [isEditingUsername, isEditingUsernameSet] = useState(false)   
     const handleLogout = () => {
         localStorage.removeItem('rexUID')
         history.go(0)
@@ -16,6 +19,21 @@ function Setting(){
 
     return(
         <div id="SettingPage">
+            <div id="username">
+                <span>Username:</span>
+                { isEditingUsername ?
+                
+                <TextField label="username" variant="outlined" size="small"value={username} onChange={(e)=>usernameSet(e.target.value)} />
+                :
+                    <span>testUsername123</span>
+                }
+                
+                
+                
+            </div>
+            <div id="profile-image-update">
+                
+            </div>
             <Button id="button" onClick={handleLogout}>LogOut</Button>
         </div>
     )
