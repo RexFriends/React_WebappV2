69db1da55d33ebc51c2355a19c7092ff055961c9
Wesley Chen <weschen.github@gmail.com>
2021-04-11 21:55:29 -0400

    styling

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { Grid } from '@material-ui/core';
-import { AddToPhotos, FileCopy, MoreHoriz, Send } from '@material-ui/icons';
-import IconButton from '@material-ui/core/IconButton';
-import TextOverflow from '../TextOverflow/TextOverflow';
-import SendIcon from '@material-ui/icons/Send';
-import APIURL from '../../assets/URL';
-import OptionsPopup from '../OptionsPopup/OptionsPopup';
-import FeedbackPopup from '../FeedbackPopup/FeedbackPopup';
-import { copyFallback } from '../../util';
-import { showAlert } from '../Alerts/Alerts';
-import AddToClosetPopup from '../AddToClosetPopup/AddToClosetPopup';
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Grid } from "@material-ui/core";
+import { AddToPhotos, FileCopy, MoreHoriz, Send } from "@material-ui/icons";
+import IconButton from "@material-ui/core/IconButton";
+import TextOverflow from "../TextOverflow/TextOverflow";
+import SendIcon from "@material-ui/icons/Send";
+import APIURL from "../../assets/URL";
+import OptionsPopup from "../OptionsPopup/OptionsPopup";
+import FeedbackPopup from "../FeedbackPopup/FeedbackPopup";
+import { copyFallback } from "../../util";
+import { showAlert } from "../Alerts/Alerts";
+import AddToClosetPopup from "../AddToClosetPopup/AddToClosetPopup";
 
 function ProductItem({ item, isOwned, updateProducts }) {
     const [hover, hoverSet] = useState(false);
     const [image, imageSet] = useState(undefined);
-    const [brand, brandSet] = useState('Brand');
-    const [productName, setProductName] = useState('Product name');
-    const [price, priceSet] = useState('$99.99');
+    const [brand, brandSet] = useState("Brand");
+    const [productName, setProductName] = useState("Product name");
+    const [price, priceSet] = useState("$99.99");
     // const [brandLogo, setBrandLogo] = useState(undefined);
     const [showPopup, setShowPopup] = useState(false);
     const [showFeedbackPopup, setShowFeedbackPopup] = useState(false);
     const [showAddToClosetPopup, setShowAddToClosetPopup] = useState(false);
     const [friends, friendsSet] = useState([]);
-    const [text, setText] = useState('');
+    const [text, setText] = useState("");
 
     useEffect(() => {
         if (item.images !== null) {
@@ -40,12 +40,12 @@ function ProductItem({ item, isOwned, updateProducts }) {
         brandSet(item.brand);
         // setBrandLogo(item.site_name);
         setProductName(item.name);
-        const itemPrice = item.price ? `${item.price}` : '';
+        const itemPrice = item.price ? `${item.price}` : "";
         priceSet(itemPrice);
     }, [item]);
 
     const getUsers = () => {
-        const rexUID = localStorage.getItem('rexUID');
+        const rexUID = localStorage.getItem("rexUID");
         fetch(`${APIURL}/api/get-users?uid=${rexUID}&text=${text}`)
             .then((res) => res.json())
             .then((json) => {
@@ -63,33 +63,33 @@ function ProductItem({ item, isOwned, updateProducts }) {
     };
 
     const handleSendRequest = (id) => {
-        const rexUID = localStorage.getItem('rexUID');
+        const rexUID = localStorage.getItem("rexUID");
         const payload = {
             user_requesting_id: id,
             product_id: item.id,
         };
         fetch(`${APIURL}/api/send_rex?uid=${rexUID}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(payload),
         })
             .catch((err) => {
-                showAlert('Failed to send', err);
+                showAlert("Failed to send", err);
             })
             .then(() => {
-                showAlert('Sent Rex!', 'success');
+                showAlert("Sent Rex!", "success");
             });
     };
 
     const handleGetCopyLink = () => {
-        const rexUID = localStorage.getItem('rexUID');
+        const rexUID = localStorage.getItem("rexUID");
         const payload = { listing_id: item.id };
         fetch(`${APIURL}/api/copy_feedback_link?uid=${rexUID}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(payload),
         })
@@ -103,7 +103,7 @@ function ProductItem({ item, isOwned, updateProducts }) {
                 } else copyFallback(link);
             })
             .then(() => {
-                showAlert('Copied link!', 'success');
+                showAlert("Copied link!", "success");
             });
     };
 
@@ -128,45 +128,47 @@ function ProductItem({ item, isOwned, updateProducts }) {
 
     const handleCloseFeedbackPopup = () => {
         setShowFeedbackPopup(false);
-        setText('');
+        setText("");
     };
 
     const handleInvite = async (nameRef, phoneRef) => {
         if (!nameRef.current || !phoneRef.current) return;
 
         try {
-            const rexUID = localStorage.getItem('rexUID');
-            const res = await fetch(`${APIURL}/api/addfriendnumber?uid=${rexUID}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name: nameRef.current.value,
-                    phonenumber: phoneRef.current.value
-                })
-            });
+            const rexUID = localStorage.getItem("rexUID");
+            const res = await fetch(
+                `${APIURL}/api/addfriendnumber?uid=${rexUID}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        name: nameRef.current.value,
+                        phonenumber: phoneRef.current.value,
+                    }),
+                }
+            );
             const json = await res.json();
             if (json.success) {
-                showAlert('Invited user!', 'success');
+                showAlert("Invited user!", "success");
                 getUsers();
-            }
-            else {
-                showAlert(`${json.reason}!`, 'error');
+            } else {
+                showAlert(`${json.reason}!`, "error");
                 return new Error(json.reason);
             }
         } catch (err) {
-            showAlert('Invite failed!', 'error');
+            showAlert("Invite failed!", "error");
             throw err;
         }
     };
 
     const handleDelete = () => {
-        const rexUID = localStorage.getItem('rexUID');
+        const rexUID = localStorage.getItem("rexUID");
         fetch(`${APIURL}/api/deletelisting?uid=${rexUID}`, {
-            method: 'POST',
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 product_id: item.id,
@@ -174,13 +176,13 @@ function ProductItem({ item, isOwned, updateProducts }) {
         })
             .then((res) => res.json())
             .then(() => {
-                showAlert('Removed product!', 'success');
+                showAlert("Removed product!", "success");
                 updateProducts();
                 setShowPopup(false);
             })
             .catch((err) => {
                 console.error(err);
-                showAlert('Removing product failed!', 'error');
+                showAlert("Removing product failed!", "error");
             });
     };
 
@@ -201,23 +203,24 @@ function ProductItem({ item, isOwned, updateProducts }) {
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ type: "tween", delay: 0.3 }}
             >
-                {/* <img style={{width: '30px', height: 'auto', borderRadius: '100%'}} src={`logo.clearbit.com/${brandLogo}`} /> */}
                 <img src={image} alt="product" id="image" />
                 <Grid
                     style={{ width: 220, padding: "0 10px" }}
                     justify="space-between"
                     container
+                    id="grid"
                 >
                     <Grid xs={8} direction="column" container item>
-            <span
-                style={{
-                    fontWeight: "bold",
-                    textAlign: "left",
-                    fontSize: "15px",
-                }}
-            >
-              {brand}
-            </span>
+                        <span
+                            style={{
+                                fontWeight: "bold",
+                                textAlign: "left",
+                                fontSize: "15px",
+                            }}
+                            id="brand"
+                        >
+                            {brand}
+                        </span>
                         <TextOverflow
                             style={{
                                 color: "rgb(114, 114, 114)",
@@ -232,7 +235,9 @@ function ProductItem({ item, isOwned, updateProducts }) {
                     </Grid>
                     <Grid xs={4} direction="column" container item>
                         <Grid id="price" item>
-                            <span style={{ fontWeight: "bold", fontSize: "15px" }}>
+                            <span
+                                style={{ fontWeight: "bold", fontSize: "15px" }}
+                            >
                                 {price}
                             </span>
                         </Grid>
@@ -256,10 +261,15 @@ function ProductItem({ item, isOwned, updateProducts }) {
                             exit={{ opacity: 0 }}
                         >
                             <div id="top">
-                                <IconButton onClick={handleShowFeedbackPopup} id="info">
+                                <IconButton
+                                    onClick={handleShowFeedbackPopup}
+                                    id="info"
+                                >
                                     <SendIcon
                                         fontSize="large"
-                                        style={{ color: "14c4b2", width: "30px", height: "30px" }}
+                                        style={{
+                                            color: "14c4b2",
+                                        }}
                                     />
                                 </IconButton>
                             </div>
@@ -267,32 +277,60 @@ function ProductItem({ item, isOwned, updateProducts }) {
                     )}
                 </AnimatePresence>
             </motion.div>
-            { isOwned ?
-            <OptionsPopup
-                anchorElementId={productId}
-                open={showPopup}
-                onClose={() => setShowPopup(false)}
-                title="Options"
-                buttons={[
-                    { text: "Add to Closet", onClick: handleShowAddToClosetPopup, icon: <AddToPhotos /> },
-                    { text: "Send a Rex", onClick: handleShowFeedbackPopup, icon: <Send />, },
-                    { text: "Copy Link", onClick: handleGetCopyLink, icon: <FileCopy /> },
-                    { text: "Remove Product", onClick: handleDelete, isDelete: true },
-                ]}
-            />
-            :
-            <OptionsPopup
-                anchorElementId={productId}
-                open={showPopup}
-                onClose={() => setShowPopup(false)}
-                title="Options"
-                buttons={[
-                    { text: "Add to Closet", onClick: handleShowAddToClosetPopup, icon: <AddToPhotos /> },
-                    { text: "Send a Rex", onClick: handleShowFeedbackPopup, icon: <Send />, },
-                    { text: "Copy Link", onClick: handleGetCopyLink, icon: <FileCopy /> },
-                ]}
-            />
-            }
+            {isOwned ? (
+                <OptionsPopup
+                    anchorElementId={productId}
+                    open={showPopup}
+                    onClose={() => setShowPopup(false)}
+                    title="Options"
+                    buttons={[
+                        {
+                            text: "Add to Closet",
+                            onClick: handleShowAddToClosetPopup,
+                            icon: <AddToPhotos />,
+                        },
+                        {
+                            text: "Send a Rex",
+                            onClick: handleShowFeedbackPopup,
+                            icon: <Send />,
+                        },
+                        {
+                            text: "Copy Link",
+                            onClick: handleGetCopyLink,
+                            icon: <FileCopy />,
+                        },
+                        {
+                            text: "Remove Product",
+                            onClick: handleDelete,
+                            isDelete: true,
+                        },
+                    ]}
+                />
+            ) : (
+                <OptionsPopup
+                    anchorElementId={productId}
+                    open={showPopup}
+                    onClose={() => setShowPopup(false)}
+                    title="Options"
+                    buttons={[
+                        {
+                            text: "Add to Closet",
+                            onClick: handleShowAddToClosetPopup,
+                            icon: <AddToPhotos />,
+                        },
+                        {
+                            text: "Send a Rex",
+                            onClick: handleShowFeedbackPopup,
+                            icon: <Send />,
+                        },
+                        {
+                            text: "Copy Link",
+                            onClick: handleGetCopyLink,
+                            icon: <FileCopy />,
+                        },
+                    ]}
+                />
+            )}
             <FeedbackPopup
                 anchorElementId={productId}
                 open={showFeedbackPopup}
